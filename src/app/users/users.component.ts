c8f005bd1fb8a0b579b994ba730e9fbd70d593f2
TONMOY RUDRA <tonmoy.rudra@technuf.com>
2018-11-12 18:38:16 +0600

    change some

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,6 +9,8 @@ export class UsersComponent implements OnInit {
   applicantList;
   constructor(public userService : UserService) { }
   mitList;
+  paidMit;
+  unpaidMit;
   pgitList;
   ngOnInit() {
   this.getAllAplicant();
@@ -17,12 +19,20 @@ export class UsersComponent implements OnInit {
   getAllAplicant(){
     this.mitList = []
     this.pgitList = []
+    this.paidMit = []
+    this.unpaidMit = []
     this.userService.getAllApplicant()
       .subscribe( result => {
         this.applicantList = result;
         for (let i = 0; i < this.applicantList.length; i++) {
           if(this.applicantList[i].applicationType == "mit"){
             this.mitList.push(this.applicantList[i])
+            if(this.applicantList[i].paymentStatus == true){
+              this.paidMit.push(this.applicantList[i])
+            }else { 
+              this.unpaidMit.push(this.applicantList[i])
+            }
+            
           }else{
             this.pgitList.push(this.applicantList[i])
           }
